1e9b50afdfc27e76b5531f1422aa3003e127bf03
Patiyut1807 <today13113@gmail.com>
2023-12-17 18:36:19 +0700

    update

--- express-app/index.js
+++ express-app/index.js
@@ -7,34 +7,56 @@ import { v4 as uuidv4 } from "uuid";
 const port = process.env.PORT || 4000;
 const app = express();
 
-// const connection = await mysql.createConnection({
-//   host: dbconfig.HOST,
-//   user: dbconfig.USER,
-//   password: dbconfig.PASSWORD,
-//   database: dbconfig.DB_NAME,
-//   port: dbconfig.DB_PORT,
-// });
-// connection.connect();
+const connection = await mysql.createConnection({
+  host: dbconfig.HOST,
+  user: dbconfig.USER,
+  password: dbconfig.PASSWORD,
+  database: dbconfig.DB_NAME,
+  port: dbconfig.DB_PORT,
+});
+connection.connect();
 app.use(json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/user",(req, res) => {
+app.get("/users", async (req, res) => {
+  let result = await connection.query("SELECT * FROM USERS");
+  res.json(result[0]);
+});
+
+app.post("/users", async (req, res) => {
   let { name, age } = req.body;
-  console.log(res.body);
-  // await connection.query(
-  //   `CREATE TABLE IF NOT EXISTS USERS(
-  //     uid VARCHAR(36) NOT NULL PRIMARY KEY,
-  //     name VARCHAR(255) NOT NULL,
-  //     age INT NOT NULL
-  // );`
-  // );
-  // await connection.query(
-  //   `INSERT INTO USERS VALUES (${uuidv4()},${name},${age});`
-  // );
-  res.send("success")
+  await connection.query(
+    `CREATE TABLE IF NOT EXISTS USERS(
+      uid VARCHAR(36) NOT NULL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      age INT NOT NULL
+  );`
+  );
+  await connection.query(
+    `INSERT INTO USERS (uid,name,age) VALUES (UUID(),?,?);`,
+    [name, age]
+  );
+  res.send("add success");
+});
+
+app.delete("/users/:uid", async (req, res) => {
+  let { uid } = req.params;
+  await connection.query(`DELETE FROM USERS WHERE uid = ?;`, [uid]);
+  res.send("remove success");
+});
+
+app.patch("/users", async (req, res) => {
+  let { uid, name, age } = req.body;
+  await connection.query(
+    `UPDATE USERS
+    SET name = ?, age = ?
+    WHERE uid = ?;`,
+    [name, age,uid]
+  );
+  res.send("update success");
 });
 
 app.listen(port, () => {
